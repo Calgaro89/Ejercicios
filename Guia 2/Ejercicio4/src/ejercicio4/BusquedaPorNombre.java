0ee712bfdaa9da62b28acdf38fbb0fb04ff79142
Calgaro89 <cristiancalgaro@gmail.com>
2023-08-16 10:06:37 -0300

    Correcciones 1.0
    Correccion de ubicacion de clases dentro del paquete.

--- Guia 2/Ejercicio4/src/ejercicio4/BusquedaPorNombre.java
+++ Guia 2/Ejercicio4/src/ejercicio4/BusquedaPorNombre.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JInternalFrame.java to edit this template
  */
-package etapa03guia01ejercicio04;
+package ejercicio4;
 
 import javax.swing.table.DefaultTableModel;
 
--- Guia 2/Ejercicio4/src/ejercicio4/Menu.java
+++ Guia 2/Ejercicio4/src/ejercicio4/Menu.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package etapa03guia01ejercicio04;
+package ejercicio4;
 
 import java.util.ArrayList;
 import java.util.TreeSet;
